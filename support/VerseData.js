b5766343acfca5d88569292a4b562c6987f90b27
Abdulaziz-MI <AM.Alzaid@outlook.com>
2023-02-12 00:13:19 +0000

    reciter name is collected
    Random reciter selected and his name is collected

--- support/VerseData.js
+++ support/VerseData.js
@@ -1,22 +1,38 @@
-
-
-
-
-
 export default class VerseData {
     constructor(verseIndex) {
         this.verseIndex = verseIndex
-}
+        this.randomReciter = this.pickRandomReciter(Object.values(this.reciters));
+        this.reciterName = Object.keys(this.reciters).find(key => this.reciters[key] === this.randomReciter);
+
+    }
 
     randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     // ********* this  will return a random verse from a specific surah number using the parametre
 
-
     // *********
     // The two params will be this by default but I plan to give users a choice.
-    async getQuranAndAudio(translation = 131, reciter = 9) {
+    reciters = {
+        "Ar-Rifai": 5,
+        "Al-Minshawi": 9,
+        "Ash-Shuraym": 10,
+        "AbdulBaset AbdulSamad": 2
+    };
+
+    pickRandomReciter(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+
+    randomReciter = this.pickRandomReciter(Object.values(this.reciters));
+
+    async getQuranAndAudio(translation = 131, reciter = this.randomReciter) {
+
+        console.log(this.randomReciter);
+
+        console.log(`The randomly selected reciter is: ${this.reciterName}`);
+
+
         const url =
             `https://api.quran.com/api/v4/verses/by_key/${this.verseIndex}?language=en
         &words=true&translations=${translation}&audio=${reciter}&fields=text_imlaei`;
@@ -35,13 +51,13 @@ export default class VerseData {
         this.verseForChallenge[this.randomWordIndex] = "____";
         this.verseForChallenge = this.verseForChallenge.join(" ");
         this.audio = `https://verses.quran.com/${json.verse.audio.url}`
-  
+
 
     }
 
     // *********
     // This will be the default param but I plan to give users a choice.
-     async getTafsir(tafsir = 14) {
+    async getTafsir(tafsir = 14) {
         const url = `https://api.quran.com/api/v4/tafsirs/${tafsir}/by_ayah/${this.verseIndex}`;
         const response = await fetch(url);
         const json = await response.json();
@@ -52,13 +68,13 @@ export default class VerseData {
 
     };
 
-async getDataFromAPI(){
-    await this.getQuranAndAudio();
-    delete this.audioFile
-    delete this.randomWordIndex
-    await this.getTafsir()
-    return this
-}
+    async getDataFromAPI() {
+        await this.getQuranAndAudio();
+        delete this.audioFile
+        delete this.randomWordIndex
+        await this.getTafsir()
+        return this
+    }
 
 
 }
